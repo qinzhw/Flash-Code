5fd1a5009ddf300c172b085414e9deeda65e5e46
qinzw <Hqcxj@1234>
2026-09-20 18:23:11 +0800

    初始化flash-code项目

---
+++ src/main/java/com/example/flashcode/FlashCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.flashcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flashcode/FlashCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flashcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
