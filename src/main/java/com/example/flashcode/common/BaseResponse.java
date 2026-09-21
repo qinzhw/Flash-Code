9721f4aa9f1dd9abbc7ecb88f2661c780e317baa
qinzhw <qinzhiwei200ok@163.com>
2026-09-21 14:42:04 +0800

    前后端代码初始化

---
+++ src/main/java/com/example/flashcode/common/BaseResponse.java
@@ -0,0 +1,34 @@
+package com.example.flashcode.common;
+
+import com.example.flashcode.exception.ErrorCode;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 基础响应包装类
+ * @param <T>
+ */
+@Data
+public class BaseResponse<T> implements Serializable {
+
+    private int code;
+
+    private T data;
+
+    private String message;
+
+    public BaseResponse(int code, T data, String message) {
+        this.code = code;
+        this.data = data;
+        this.message = message;
+    }
+
+    public BaseResponse(int code, T data) {
+        this(code, data, "");
+    }
+
+    public BaseResponse(ErrorCode errorCode) {
+        this(errorCode.getCode(), null, errorCode.getMessage());
+    }
+}
